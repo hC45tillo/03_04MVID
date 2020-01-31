4b6dafaebbfe80d0f5cad0d729c7a16fc6365865
Hugo <Castillo>
2020-01-31 01:14:06 -0600

    EJ02.05
    Done

---
+++ projects/EJ02_06/main.cpp
@@ -0,0 +1,155 @@
+#include <glad/glad.h>
+#include "engine/window.hpp"
+#include <iostream>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+void handleInput() {}
+
+bool checkShader(uint32_t shader) {
+	int success;
+	char infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+		std::cout << "Error Compiling Shader " << infoLog << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool checkProgram(uint32_t program) {
+	int success;
+	char infoLog[512];
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(program, 512, nullptr, infoLog);
+		std::cout << "Error Linking Program " << infoLog << std::endl;
+		return false;
+	}
+	return true;
+}
+
+uint32_t createProgram() {
+	const char* vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main() {\n"
+		"	gl_Position = vec4(aPos, 1.0);\n"
+		"}\0";
+
+	const char* fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main() {\n"
+		"	FragColor = vec4(0.0, 1.0, 0.0, 1.0);\n"
+		"}\0";
+
+	const uint32_t vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+	glCompileShader(vertexShader);
+	checkShader(vertexShader);
+
+	const uint32_t fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+	glCompileShader(fragmentShader);
+	checkShader(fragmentShader);
+
+	const uint32_t program = glCreateProgram();
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, fragmentShader);
+	glLinkProgram(program);
+	checkProgram(program);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return program;
+}
+
+uint32_t createVertexData(uint32_t* VBO, uint32_t* EBO) {
+	
+	int sides = 6;
+	float polygon[7 * 3] = {};
+	int idx = 3;
+	for (int i = 0; i < 6; i++) {
+		polygon[idx] = 1 * (float)cos(i * 60 * M_PI / 180.f);
+		idx++;
+		polygon[idx] = 1 * (float)sin(i * 60 * M_PI / 180.f);
+		idx++;
+		polygon[idx] = 0.f;
+		idx++;
+	}
+	
+	uint32_t indices[6 * 3] = {	};
+	idx = 0;
+	for (int i = 0; i < sides; i++) {
+		indices[idx] = 0;
+		idx++;
+		indices[idx] = i + 1;
+		idx++;
+		if (i < sides - 1) {
+			indices[idx] = i + 2;
+		}
+		else {
+			indices[idx] = 1;
+		}
+		idx++;
+	};
+
+	uint32_t VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, VBO);
+	glGenBuffers(1, EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, *VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(polygon), polygon, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	return VAO;
+}
+
+void render(uint32_t VAO, uint32_t program) {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(program);
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, nullptr);
+}
+
+int main(int, char* []) {
+	Window* window = Window::instance();
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	uint32_t VBO, EBO;
+	const uint32_t VAO = createVertexData(&VBO, &EBO);
+	const uint32_t program = createProgram();
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+
+	while (window->alive()) {
+		handleInput();
+		render(VAO, program);
+		window->frame();
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
+	glDeleteProgram(program);
+
+	return 0;
+}
