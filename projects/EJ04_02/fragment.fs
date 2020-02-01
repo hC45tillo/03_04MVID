c66d5faf353221f8c0b645f3a2f2383c0d37eeb9
Hugo <Castillo>
2020-01-31 20:31:33 -0600

    EJ04.02
    Done

---
+++ projects/EJ04_02/fragment.fs
@@ -0,0 +1,13 @@
+#version 330 core
+
+in vec3 myColor;
+in vec2 UV;
+out vec4 FragColor;
+
+
+uniform sampler2D tex_1;
+uniform vec3 addColor;
+
+void main() {
+    FragColor = texture(tex_1, UV) * vec4(myColor + addColor, 1.0);
+}
