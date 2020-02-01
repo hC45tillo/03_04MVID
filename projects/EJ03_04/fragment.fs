6a7b0743552e8f47ff72263b316bafcd59e3bd3e
Hugo <Castillo>
2020-01-31 18:11:43 -0600

    EJ03.04
    Done

--- projects/EJ03_04/fragment.fs
+++ projects/EJ03_04/fragment.fs
@@ -3,11 +3,8 @@
 in vec3 myColor;
 out vec4 FragColor;
 
-in vec3 aPosf;
-
 uniform vec3 addColor;
 
 void main() {
     FragColor = vec4(myColor + addColor, 1.0);
-    gl_Position = vec4(aPosf, 1.0);
 }
