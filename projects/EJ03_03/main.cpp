aed8066df9c8edaa068306a37d7a2db20303a246
Hugo <Castillo>
2020-01-31 18:07:27 -0600

    EJ03.03
    Done

---
+++ projects/EJ03_03/main.cpp
@@ -0,0 +1,88 @@
+#include <glad/glad.h>
+
+#include "engine/window.hpp"
+#include "engine/shader.hpp"
+
+#include <GLFW/glfw3.h>
+
+void handleInput() {}
+
+uint32_t createVertexData(uint32_t* VBO, uint32_t* EBO) {
+	float vertices[] = {
+		 0.5f, -0.5f, 0.0f,      1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f,      0.0f, 1.0f, 0.0f,
+		 0.0f,  0.5f, 0.0f,      0.0f, 0.0f, 1.0f
+	};
+
+	uint32_t indices[]{
+		0, 2, 1
+	};
+
+	uint32_t VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, VBO);
+	glGenBuffers(1, EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, *VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	return VAO;
+}
+
+void render(uint32_t VAO, const Shader& shader) {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	shader.use();
+	shader.set("addColor", 0.2f, 0.f, 0.f);
+
+	int time = glfwGetTime();
+	//if (time % 2 == 0) {
+		shader.set("moveObj", 1.0f/(time/2), 0.f, 0.f);
+	//}
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+}
+
+int main(int, char* []) {
+	Window* window = Window::instance();
+
+	glClearColor(0.f, 0.f, 0.f, 1.0f);
+
+	uint32_t VBO, EBO;
+	const uint32_t VAO = createVertexData(&VBO, &EBO);
+	const Shader shader("../projects/EJ03_03/vertex.vs", "../projects/EJ03_03/fragment.fs");
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+
+	while (window->alive()) {
+		handleInput();
+		render(VAO, shader);
+		window->frame();
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
+	return 0;
+}
